4e795d25607f089b1291fe9193a17b4fbd9785f5
Prashant Jha <Prashant Jha@DESKTOP-P66H5H9>
2020-08-08 12:37:14 +0530

    updated

--- cms/src/main/java/com/ciss/cms/CmsApplication.java
+++ cms/src/main/java/com/ciss/cms/CmsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CmsApplication {
-
+//demo project
 	public static void main(String[] args) {
 		SpringApplication.run(CmsApplication.class, args);
 		
